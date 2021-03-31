f3560499687db3f3440747b62080f1a660950713
simonmysun <simonmysun@gmail.com>
2021-03-31 05:41:30 +0200

    update

---
+++ dotfiles/link.sh
@@ -0,0 +1,6 @@
+ln -s /home/mysun/git/simonmysun/praxis/dotfiles/.Xmodmap ~/.Xmodmap
+ln -s /home/mysun/git/simonmysun/praxis/dotfiles/.vimrc ~/.vimrc
+ln -s /home/mysun/git/simonmysun/praxis/dotfiles/.bashrc ~/.bashrc
+ln -s /home/mysun/git/simonmysun/praxis/dotfiles/.bashrcd ~/.bashrcd
+ln -s /home/mysun/git/simonmysun/praxis/dotfiles/.tmux.conf ~/.tmux.conf
+
